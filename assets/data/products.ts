4932bbb2dc798daf4b485c7ccbc295abe769d03a
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2024-09-30 15:35:13 +0530

    building UI

---
+++ assets/data/products.ts
@@ -0,0 +1,34 @@
+const products = [
+    {
+      id: 1,
+      name: 'Ultimate Pepperoni',
+      image: 'https://media.istockphoto.com/id/1473906544/photo/close-up-on-delicious-round-pizza-with-prosciutto-mozzarella-and-basil-leaves-on-top-served.jpg?s=1024x1024&w=is&k=20&c=AYzUIoHYw6a_dYMEUloHOCQXueOUW8bzgPTiP1C0Uqo=',
+      price: 12.99,
+    },
+    {
+      id: 2,
+      name: 'Extravaganzza',
+      image: 'https://media.istockphoto.com/id/153784617/photo/supreme-pizza-slice-lift.jpg?s=1024x1024&w=is&k=20&c=IsJ59Vcow4V-VfxTZPAZD27Tey6KDsUpDCJFljzSFwA=',
+      price: 14.99,
+    },
+    {
+      id: 3,
+      name: 'Meatzza',
+      image: 'https://media.istockphoto.com/id/489751583/photo/supreme-italian-pizza-with-pepperoni-and-toppings.jpg?s=1024x1024&w=is&k=20&c=Xm3MzXZXJrjKinQ25mXXnrxdvgjQQnXpchiHTdL0Rm4=',
+      price: 16.99,
+    },
+    {
+      id: 4,
+      name: 'Veggie Delight',
+      image: 'https://media.istockphoto.com/id/515834876/photo/pizza-spinach-and-feta.jpg?s=1024x1024&w=is&k=20&c=kZezuOaJS8IsUN-mVkSUISGU-R7ORSpglLHAWjq0om8=',
+      price: 11.49,
+    },
+    {
+      id: 5,
+      name: 'Hawaiian Classic',
+      image: 'https://media.istockphoto.com/id/1065169946/photo/big-pizza-with-cheese-tomatoes-black-olives-and-paprika-on-a-round-cutting-board-on-a-dark.jpg?s=1024x1024&w=is&k=20&c=cWvBoPYlwSE6UAP6I8peTCLtQZsoGJe9QaH9QhcSBsQ=',
+      price: 13.99,
+    },
+  ];
+  
+export default products;
