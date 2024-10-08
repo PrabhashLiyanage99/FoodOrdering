e9e38de7267300f5ce1dc955d59d94a67cb49eb5
PrabhashLiyanage99 <159533393+PrabhashLiyanage99@users.noreply.github.com>
2024-10-08 17:33:54 +0530

    total-price-implementation

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -5,17 +5,20 @@ import { useContext } from 'react';
 import { CartContext, useCart } from '../providers/CartProvider';
 import { FlatList } from 'react-native';
 import CartListItem from '../components/CartListItem';
+import Button from '../components/Button';
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total  } = useCart();
   return (
-    <View>
+    <View style={{padding:10 }}>
       <FlatList 
         data={items} 
         renderItem={({ item }) => <CartListItem cartItem={item} />}
-        contentContainerStyle={{padding:10 , gap: 10}}
+        contentContainerStyle={{ gap: 10}}
         //keyExtractor={(item) => item.id.toString()} // Add a keyExtractor to avoid warnings
       />
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: '500' }}>Total: ${total}</Text>
+      <Button text="Checkout" />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
